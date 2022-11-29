519025578cfc3faab72424ace0b4acd75dc7e9e9
rmyau <katya.anishenko24@gmail.com>
2022-11-29 19:52:31 +0300

    Added decompress

--- IFS.py
+++ IFS.py
@@ -3,22 +3,33 @@ from scipy import ndimage
 import matplotlib.image as mpigm
 import matplotlib.pyplot as plt
 from rangTree import *
+import math
 
 
 class cIFS:
+
+
     def __init__(self, file_name='', max_tree_depth=1, eps=0.0, maxLevelDomain=0):
+        self.file_name = file_name
         # уровень макс достигнутый в дереве
         self.max_fract = 0
         # погрешность
         self.eps = eps
-        self.file_name = file_name
+        self.domain_list = []
         # размерность блоков рангов на нулевом уровне
-        self.max_tree_depth = max_tree_depth
+        self.max_tree_depth = max_tree_depth - 1
         self.maxlevelDomain = maxLevelDomain
         # значения афинных преобразований
-        self.directions = [1, -1]  # кооэфициент для отражения
-        self.angles = [0, 90, 180, 270]  # углы для афинных преобразований
-        self.candidates = [[direction, angle] for direction in self.directions for angle in self.angles]
+        directions = [1, -1]  # кооэфициент для отражения
+        angles = [0, 90, 180, 270]  # углы для афинных преобразований
+        self.candidates = [[direction, angle] for direction in directions for angle in angles]
+        if maxLevelDomain == 0:
+            self.decompress()
+        else:
+            self.compress()
+        self.startSizeRange = None
+        self.height = None
+        self.width = None
 
     @staticmethod
     def rotate(img, angle):
@@ -33,13 +44,11 @@ class cIFS:
     def flip(img, direction):
         return img[::direction, :]
 
-    def apply_transformation(self, img, direction, angle):
-        return self.rotate(self.flip(img, direction), angle)
+    def apply_transformation(self, img, direction, angle, contrast=1.0, brightness = 0.0):
+        return contrast*self.rotate(self.flip(img, direction), angle)+brightness
 
-    def reduce(self, img, factor):  # img - в данном случае для домена это data
-        # двумерный массив, заполненный нулями, соответствует размерности изображения
-        # img.shape[0] - количество пикселей
-
+    @staticmethod
+    def reduce(img, factor):  # img - в данном случае для домена это data
         result = np.zeros((img.shape[0] // factor, img.shape[1] // factor))
         for i in range(result.shape[0]):
             for j in range(result.shape[1]):
@@ -50,6 +59,7 @@ class cIFS:
     @staticmethod
     def brightness_contrast(rang, domain):
         size = rang.size
+        print(rang, domain)
         dataD = np.array(domain)
         dataR = np.array(rang.data)
         sumD = np.sum(dataD)
@@ -70,7 +80,14 @@ class cIFS:
         s = a / b
         return s, o
 
-    # создание вектора доменов и рангов
+    def find_contrast_and_brightness2(self, rang, domain):
+        # Fit the contrast and the brightness
+        A = np.concatenate((np.ones((domain.size, 1)), np.reshape(domain, (domain.size, 1))), axis=1)
+        b = np.reshape(rang, (rang.size,))
+        x, _, _, _ = np.linalg.lstsq(A, b)
+        # x = optimize.lsq_linear(A, b, [(-np.inf, -2.0), (np.inf, 2.0)]).x
+        return x[1], x[0]
+        # создание вектора доменов и рангов
     def get_rang(self, size, step, level, isRang):  # int,int,bool
         w = self.width
         h = self.height
@@ -79,7 +96,6 @@ class cIFS:
             w -= step
         vector = []
         indexRang = 0
-        print(f'step = {step}, h={h}, w={w}, size={size}')
         for y in range(0, h, step):
             for x in range(0, w, step):
                 indexRang += 1
@@ -89,16 +105,20 @@ class cIFS:
                 if isRang:
                     block.arr = byte(self.max_tree_depth).arr
                     block.arr[0] = indexRang
-                    print(f'block arr {block.arr}')
                 vector.append(block)
         return vector
+
+    def get_all_domain(self):
+        sD = self.startSizeRange
+        for level in range(self.maxlevelDomain - 1):
+            self.domain_list.extend(self.get_rang(sD, sD // 2, level, False))
+            sD //= 2
 
     def new_level_rang(self, rang):
         nextLevelTree = rang.get_next_level_tree()
         newSize = rang.size // 2
         newCoord = [(rang.x, rang.y), (rang.x, rang.y + newSize), (rang.x + newSize, rang.y),
                     (rang.x + newSize, rang.y + newSize)]
-        print(newCoord)
         newLev = rang.level + 1
         for j in range(4):
             newX = newCoord[j][0]
@@ -109,50 +129,44 @@ class cIFS:
             self.levelRange[newLev].append(new_rang)
 
     def find_best_transformation(self, rang, domain, minEps, bestDomain):
+        print('NEXT DOMAIN--')
+        print(f'in best tr x={domain.x} y={domain.y}')
         for direction, angle in self.candidates:
             convDomain = self.apply_transformation(domain.data, direction, angle)
             convDomain = self.reduce(convDomain, domain.size // rang.size)  # сжатый измененный домен
             # найдем контраст и яркость
-            s, o = self.brightness_contrast(rang, convDomain)
+            # s, o = self.brightness_contrast(rang, convDomain)
+            s,o = self.find_contrast_and_brightness2(rang.data, convDomain)
             convDomain = convDomain * s + o
             # высчитываем разницу попиксельно
-            epsD = np.sum(np.square(rang.data, convDomain)) / (rang.size * rang.size)
+
+            epsD = np.sum(np.square(rang.data - convDomain)) / (rang.size * rang.size)
             if epsD < minEps:
-                minEps = epsD
+                minEps = float(epsD)
+                print(f'coord x={domain.x} y={domain.y}')
                 bestDomain = (domain.x, domain.y, domain.size, direction, angle, s, o)
         return minEps, bestDomain
-
-    def get_all_domain(self):
-        domain_list = []
-        sD = self.startSizeRange
-        for level in range(self.maxlevelDomain - 1):
-            domain_list.extend(self.get_rang(sD, sD // 2, level, False))
-            sD //= 2
-        return domain_list
 
     def find_domain_for_rang(self, rang):
         findDomain = False
         # погрешность
         bestDomain = 0
-        minEps = float('inf')
+        minEps = 2.0
         # сначала изменяем домен, потом его сжимаем, потом проверяем на соответствие
-        for domain in self.domain_list:
+        for ind in range(len(self.domain_list)):
+            domain = self.domain_list[ind]
             if domain.size > rang.size:
                 # рассмотрим все преобразования для домена
+                # minEps, bestDomain = self.find_best_transformation(rang, domain, minEps, bestDomain)
                 minEps, bestDomain = self.find_best_transformation(rang, domain, minEps, bestDomain)
 
                 if minEps < self.eps:
                     rang.domainSource = bestDomain
                     findDomain = True
-                    print(f'find domain eps -- {minEps}')
-                    break
-        print(f'eps  ----  {minEps}')
         if findDomain is False:
-            print('Разбиваем на новый уровень')
             if rang.level < self.max_tree_depth - 1:
                 self.new_level_rang(rang)
             else:
-                print('find domain')
                 rang.domainSource = bestDomain
 
     def compress(self):  # eps - погрешность
@@ -170,7 +184,7 @@ class cIFS:
         for rang in self.levelRange[0]:
             self.new_level_rang(rang)
 
-        domain_list = self.get_all_domain()
+        self.get_all_domain()
         # обработка
         for i in range(1, self.max_tree_depth):
             # если разбиения на этом уровне нет, то закончить обработку
@@ -199,6 +213,90 @@ class cIFS:
                     output.write(self.get_info_rang(rang).encode())
         output.close()
 
+    def find_size_vector(self):
+        vector_size = []
+        s = self.startSizeRange
+        for i in range(self.max_tree_depth):
+            vector_size.append(s)
+            s //= 2
+        return vector_size
+
+    def find_coordinates_rang(self, index_rang_arr):
+        x, y = 0, 0
+        size_level = self.startSizeRange
+        for ind in index_rang_arr:
+            if ind == 0:
+                break
+            if ind == 1 or ind == 2:
+                x += size_level
+            if ind == 2 or ind == 4:
+                y += size_level
+            size_level //= 2
+        return [x, y, size_level*2]
+
+    def decompress(self):
+        info_rangs = self.read_file()
+        old_image = [np.random.randint(0, 256, (self.height, self.width))]
+        size_level_rang = self.find_size_vector()
+        for iter_ in range(6):
+            new_image = np.zeros((self.width, self.height))
+            print(iter_)
+            #[xr,yr,sr], xd, yd, sd, direction, angle, s, 0
+            for info in info_rangs:
+                rang_x, rang_y, rang_size = int(info[0][0]), int(info[0][1]), int(info[0][2])
+                # print(rang_x, rang_y, rang_size)
+                domain_x, domain_y, domain_size = int(info[1]), int(info[2]), int(info[3])
+                direction, angle = int(info[4]), int(info[5])
+                s, o = float(info[6]), float(info[7])
+                domain_data = old_image[-1][domain_y:domain_y+domain_size, domain_x:domain_x+domain_size]
+                convDomain = self.apply_transformation(domain_data, direction, angle, s, o)
+                convDomain = self.reduce(convDomain, domain_size // rang_size)
+
+                new_image[rang_y:rang_y+rang_size, rang_x:rang_x+rang_size] = convDomain
+            print(new_image)
+            old_image.append(new_image)
+        self.plot_iterations(old_image)
+        plt.show()
+
+    def plot_iterations(self, iterations, target=None):
+        # Configure plot
+        plt.figure()
+        nb_row = math.ceil(np.sqrt(len(iterations)))
+        nb_cols = nb_row
+        # Plot
+        for i, img in enumerate(iterations):
+            plt.subplot(nb_row, nb_cols, i + 1)
+            plt.imshow(img, cmap='gray', vmin=0, vmax=255, interpolation='none')
+            if target is None:
+                plt.title(str(i))
+            else:
+                # Display the RMSE
+                plt.title(str(i) + ' (' + '{0:.2f}'.format(np.sqrt(np.mean(np.square(target - img)))) + ')')
+            frame = plt.gca()
+            frame.axes.get_xaxis().set_visible(False)
+            frame.axes.get_yaxis().set_visible(False)
+        plt.tight_layout()
+
+
+
+    def show_image(self, img):
+        plt.figure()
+        plt.imshow(img, cmap='gray', vmin=0, vmax=255, interpolation='none')
+        plt.show()
+    def read_file(self):
+        file = open(self.file_name, 'rb')
+        size = [int(x) for x in file.readline().split()]
+        self.width, self.height = size[0], size[1]
+        self.startSizeRange = self.width // 2
+        info_file = [x.split() for x in file.readlines()]
+        file.close()
+        self.max_tree_depth = len(info_file[0][0])
+        for i in range(len(info_file)):
+            arr_rang = [int(x) for x in list(str(int(info_file[i][0])))]
+            info_file[i][0] = self.find_coordinates_rang(arr_rang)
+
+        return info_file
+
 
     @staticmethod
     def checkImage(img, startSizeRange):
@@ -210,7 +308,7 @@ class cIFS:
 
 
 # test_ifc = cIFS("monkey.gif", 5, 0.4, 4)
-test_ifc = cIFS("monkey_256_grey.png", 5, 0.4, 4)
-print(f'start size {test_ifc.startSizeRange}')
-
-test_ifc.compress()
+# test_ifc = cIFS("monkey.png", 5, 0.005, 4)
+# print(f'start size {test_ifc.startSizeRange}')
+decode_ifc = cIFS(file_name='monkey.fbr')
+# test_ifc.compress()
--- rangTree.py
+++ rangTree.py
@@ -1,41 +1,34 @@
 import numpy as np
+
+
 # структура ранга
 class range_struct:
     def __init__(self, x, y, size, step, level, data):
-        #массив с номером в дереве для ранга
+        # массив с номером в дереве для ранга
         self.arr = 0
-        #self.domain_index = -1 - добавить в процессе обработки
-        self.x=x
-        self.y=y
-        self.size=size
-        self.step=step
+        # self.domain_index = -1 - добавить в процессе обработки
+        self.x = x
+        self.y = y
+        self.size = size
+        self.step = step
         self.level = level
-        #массив изображения в растре для заданного блока
+        # массив изображения в растре для заданного блока
         self.data = data
-        #для рангов
-        self.domainSource = 0 #хранит координаты доменного блока, размер, отражение, угол, контраст, яркость
-        #если данный показатель истинный - ранг не храним в сжатом файле
+        # для рангов
+        self.domainSource = 0  # хранит координаты доменного блока, размер, отражение, угол, контраст, яркость
+        # если данный показатель истинный - ранг не храним в сжатом файле
         self.haveNextLevel = False
-
-    def set_raster(self):
-        data = [[] for j in range(self.size)]
-        print(f'size in raster {self.size}, x= {self.x}, y = {self.y}')
-        for i in range(self.size):
-            for j in range(self.size):
-                # print(f'i = {i},  j = {j}')
-                data[i].append(self.img[self.x + i, self.y + j])
-        return data
 
     def get_next_level_tree(self):
         self.haveNextLevel = True
-        #добавить исключения
-        if self.arr==0:
+        # добавить исключения
+        if self.arr == 0:
             print('Error in rang')
             return
         vector = []
         for i in range(1, 5):
             b = self.arr.copy()
-            b[self.level+1] = i
+            b[self.level + 1] = i
             vector.append(b)
         print(f'vector {vector}')
         return vector
@@ -45,7 +38,6 @@ class range_struct:
 class byte:
     def __init__(self, depth):
         self.arr = [0 for i in range(depth)]
-
 
 # levelArray = []
 # # мощность уровня
